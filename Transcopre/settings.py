b8dc6b91b73ff138dade2eb919f0daa0dbc420e3
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2023-02-13 13:20:11 +0100

    secret key

--- Transcopre/settings.py
+++ Transcopre/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates" )
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "django-insecure--8fw+&xzo#3s79f5v2wk3+apekiub4exv!6=sdxb8rf%mc@rg2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
